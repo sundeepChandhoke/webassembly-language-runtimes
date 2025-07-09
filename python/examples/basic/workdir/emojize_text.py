d738c2d8467f8f76414c008379295dd712b3ec7c
Sundeep Chandhoke <31551631+sundeepChandhoke@users.noreply.github.com>
2025-07-09 13:56:49 -0500

    Update emojize_text.py

--- python/examples/basic/workdir/emojize_text.py
+++ python/examples/basic/workdir/emojize_text.py
@@ -10,8 +10,8 @@ Prints on stdout if no OUTPUT_FILE is given.
 Usage:
 emojize_text.py SOURCE_FILE [OUTPUT_FILE]
 """
-import sys;
-from emoji import unicode_codes;
+import sys
+from emoji import emojize
 
 # Open source file
 source_file_path = sys.argv[1]
@@ -21,14 +21,25 @@ source_file = open(source_file_path, 'r')
 out_file = open(sys.argv[2], 'w') if len(sys.argv) > 2 else sys.stdout
 
 # Transform text
-emoji_dict = unicode_codes.get_emoji_unicode_dict('en')
 output = ''
 for line in source_file.readlines():
     output_line = []
     for word in line.split():
-        output_line.append(emoji_dict.get(f':{word}:', word))
-    out_file.write(" ".join(output_line))
+        # Try to convert the word to an emoji using the format :word:
+        emoji_word = emojize(f':{word}:', language='en')
+        # If the word wasn't converted (still has colons), use the original word
+        if emoji_word == f':{word}:':
+            output_line.append(word)
+        else:
+            output_line.append(emoji_word)
+    line_text = " ".join(output_line)
+    out_file.write(line_text)
     out_file.write('\n')
+    # Accumulate output for the final print
+    output += line_text + '\n'
+
+# Print output
+print(output)
 
 # Print output
 print(output)
